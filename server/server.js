a807de0538233b6f9cca3d2b7f11a0ddf17a5726
Andrew <afuselier23@gmail.com>
2018-06-21 17:26:15 -0700

    Add DELETE /expense/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db');
 const { Expense } = require('./model');
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// Create a new expense
 app.post('/expenses', (req, res) => {
   const expense = new Expense({
     description: req.body.description,
@@ -23,11 +25,30 @@ app.post('/expenses', (req, res) => {
   .catch(err => res.status(400).send(err));
 });
 
+// Get all expenses 
 app.get('/expenses', (req, res) => {
   Expense.find().then((expenses) => {
     res.send({ expenses });
   }, (err) => {
     res.status(400).send(err);
+  });
+});
+
+// Delete an expense
+app.delete('/expenses/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Expense.findByIdAndRemove(id).then((expense) => {
+    if (!expense) {
+      return res.status(404).send();
+    }
+    res.send(expense);
+  }).catch((err) => {
+    res.status(400).send();
   });
 });
 
