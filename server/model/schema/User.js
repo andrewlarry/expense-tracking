1dc5df9d2f4e5994e69038ebc7882570c4ac92ff
Andrew <afuselier23@gmail.com>
2018-06-21 18:23:32 -0700

    created User schema and model

--- server/model/schema/User.js
+++ server/model/schema/User.js
@@ -1,7 +1,33 @@
 const { Schema } = require('mongoose');
+const valiator = require('validator');
 
 const userSchema = new Schema({
-
+  email: {
+    type: String, 
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: valiator.isEmail(),
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 module.exports = userSchema;
